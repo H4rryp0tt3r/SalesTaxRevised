bb7905df51e126b015a73a3f24b63525b4971bed
H4rryp0tt3r <nagesh.podilapu@gmail.com>
2015-06-15 11:53:11 +0530

    Nagesh - Edited build.xml by chaning Output Jar name to SalesTax.jar

--- test/com/twu/salestax/SalesTaxCalculatorTest.java
+++ test/com/twu/salestax/SalesTaxCalculatorTest.java
@@ -17,7 +17,7 @@ public class SalesTaxCalculatorTest {
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-    private final ByteArrayInputStream inContent = new ByteArrayInputStream("1 imported box of chocolates at 10.00\n1 imported bottle of perfume at 47.50".getBytes());
+    private final ByteArrayInputStream inContent = new ByteArrayInputStream("1 book at 12.49\n1 music CD at 14.99\n1 chocolate bar at 0.85".getBytes());
 
     @Before
     public void setUpStreams() {
@@ -42,10 +42,11 @@ public class SalesTaxCalculatorTest {
 
         String actualResponse = outContent.toString();
 
-        assertThat(actualResponse, is("1 imported box of chocolates: 10.50\n" +
-                "1 imported bottle of perfume: 54.63\n" +
-                "Sales Taxes: 7.63\n" +
-                "Total: 65.13\n"));
+        assertThat(actualResponse, is("1 book: 12.49\n" +
+                "1 music CD: 16.49\n" +
+                "1 chocolate bar: 0.85\n" +
+                "Sales Taxes: 1.50\n" +
+                "Total: 29.83\n"));
     }
 
     @After
